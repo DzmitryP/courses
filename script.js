187ec7edb19c90241f1ca4236c5e93a03bd79742
DzmitryP <47824937+DzmitryP@users.noreply.github.com>
2019-11-15 15:13:36 +0300

    Create script.js
    add script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+// function isThereCat(animals) {
+//   if (animals.toLowerCase().indexOf("cat") == -1) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+// console.log(isThereCat("dog, zebra, dog, cat"));
+// console.log(isThereCat("dog, zebra, dog"));
+
+// function gentleSum(a, b) {
+//   if (parseInt(a) || parseInt(b) === NaN) {
+//     return parseInt(a) + parseInt(b);
+//   } else {
+//     return "please enter value data";
+//   }
+// }
+// console.log(gentleSum("122sfdf", 5));
+// console.log(gentleSum("Hello World", 5));
+// console.log(gentleSum("234asdfsdf", "232sdffgff"));
+// console.log(gentleSum(7, 5));
+
+function gentleSum(a, b) {
+  let c = parseInt(a);
+  let d = parseInt(b);
+  if (isNaN(c) || isNaN(d)) {
+    return "Please enter valid data";
+  } else {
+    return c + d;
+  }
+}
+console.log(gentleSum("122sfdf", 5));
+console.log(gentleSum("Hello World", 5));
+console.log(gentleSum("234asdfsdf", "232sdffgff"));
+console.log(gentleSum(7, 5));
